be49432008684f2562cad357f22d29d77e284eb0
Andrey Patseyko <andrey.patseyko@cactussoft.biz>
2018-10-29 08:18:11 +0300

    start project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log('test update')
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+var my_canvas = document.getElementById("canvas");
+var style = window.getComputedStyle(my_canvas);
+var renderer = new THREE.WebGLRenderer({ canvas: my_canvas });
+console.log(style.height)
+console.log(style.width)
+renderer.setSize(parseInt(style.width), parseInt(style.height));
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+var animate = function() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
