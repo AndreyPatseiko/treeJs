f57eb41b64ba172ce1ca713b6dd10fa7c67ff84d
AndreyPatseiko <a.patseiko@gmail.com>
2018-11-05 22:02:01 +0300

    add orbit controls

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
 var my_canvas = document.getElementById("canvas");
 var style = window.getComputedStyle(my_canvas);
 var renderer = new THREE.WebGLRenderer({ canvas: my_canvas });
-console.log(style.height)
-console.log(style.width)
 renderer.setSize(parseInt(style.width), parseInt(style.height));
 renderer.setClearColor(0x000000);
 document.body.appendChild(renderer.domElement);
@@ -20,40 +11,39 @@ var material = new THREE.MeshBasicMaterial({ color: 0xff00ff, side: THREE.Double
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+//──── CAMERA ────────────────────────────────────────────────────────────────────────────
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 camera.position.z = 5;
 
-
-
-
-// LIGHT
+//──── LIGHT ─────────────────────────────────────────────────────────────────────────────
 // ambient
 var ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight);
 
-// MODEL
+//──── 3D OBJECTS ────────────────────────────────────────────────────────────────────────
 var loader = new THREE.JSONLoader();
 loader.load('http://localhost:3000/models/test_3p.js', function (geometry, materials) {
   material.morphTargets = true;
   var mesh = new THREE.Mesh(geometry, materials);
-  // scene.add(mesh);
-
+  scene.add(mesh);
 });
 
-controls = new THREE.TrackballControls(camera);
-controls.rotateSpeed = 1.0;
-controls.zoomSpeed = 1.2;
-controls.panSpeed = 0.8;
-controls.noZoom = false;
-controls.noPan = false;
-controls.staticMoving = true;
-controls.dynamicDampingFactor = 0.3;
+//──── Controls ──────────────────────────────────────────────────────────────────────────
+controls = new THREE.OrbitControls(camera);
+controls.update();
 
-// RENDER LOOP
+//──── RENDER LOOP ───────────────────────────────────────────────────────────────────────
 var animate = function () {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
+  controls.update();
   renderer.render(scene, camera);
 };
 
